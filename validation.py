f84434c960974877f2da2c452395cfc5287e5d5b
Christopher Atkins <chatki@iu.edu>
2022-04-29 19:44:12 -0400

    naive similarity walkforward validation

--- validation.py
+++ validation.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.metrics import mean_absolute_error, max_error
-from MISO_data import prior_load_colname, WEATHER_STATIONS
+from MISO import prior_load_colname, WEATHER_STATIONS
 
 def mtlf_predict_window(model, X_test, forecasts = None):
     window_size = X_test.shape[0]
